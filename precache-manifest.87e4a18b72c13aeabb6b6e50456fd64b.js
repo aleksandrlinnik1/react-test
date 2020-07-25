168e4d6342cab955fe152b46bbaa9237bacc1b75
aleksandrlinnik1 <aleksandrlinnik1@gmail.com>
2020-07-25 21:24:43 +0300

    Updates

---
+++ precache-manifest.87e4a18b72c13aeabb6b6e50456fd64b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba1abc5b45a776c202a8143ebf8e7c9",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "7ebee8646b3220e56b44",
+    "url": "/react-test/static/css/main.af1a14dd.chunk.css"
+  },
+  {
+    "revision": "b2e538446496e96f85e3",
+    "url": "/react-test/static/js/2.6e347699.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-test/static/js/2.6e347699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ebee8646b3220e56b44",
+    "url": "/react-test/static/js/main.bd4428a8.chunk.js"
+  },
+  {
+    "revision": "8552d2ec2bdfe3d4216d",
+    "url": "/react-test/static/js/runtime-main.16283f8b.js"
+  }
+]);
